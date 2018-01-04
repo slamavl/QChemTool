6de2b8edf79c3610bebc1fd59be3e102856a0b83
Vladislav Slama <slamavl@gmail.com>
2018-01-04 17:14:46 +0100

    RESP extended output and calculation of multipole moments

--- QChemTool/QuantumChem/esp.py
+++ QChemTool/QuantumChem/esp.py
@@ -12,7 +12,7 @@ from .Classes.general import Coordinate
 
 def RESP_fit_charges(struc,ESP_coor,ESP_pot, Q_tot=0.0, restr=0.001, H_fit=True,
                      List=None, MaxInt=100, q_tol=1e-6, NoHrestr=True, 
-                     constrains=[], Type = "Ground"):
+                     constrains=[], Freeze=[], Type = "Ground"):
     """ Function for restrained ESP fit of atomic center charges from 
     electrostatic potential
     
@@ -53,7 +53,7 @@ def RESP_fit_charges(struc,ESP_coor,ESP_pot, Q_tot=0.0, restr=0.001, H_fit=True,
         hydrogens
     constrains : list of list of integer (optional init = [])
         List of atoms with the same charges (e.g. for symmetry or other reasons).
-        For example `constrains=[[1,5,8,10],[2,6]]` means that q[1]=q[5]=q[8]=q[10]
+        For example ``constrains=[[1,5,8,10],[2,6]]`` means that q[1]=q[5]=q[8]=q[10]
         and q[2]=q[6]. For using constrains it is recommended to use the whole
         structure for fitting because after exluding the hydrogens indexes might
         change. Indexing is from zero!
@@ -135,13 +135,18 @@ def RESP_fit_charges(struc,ESP_coor,ESP_pot, Q_tot=0.0, restr=0.001, H_fit=True,
     C = np.ones((1,Ncharge),dtype='f8')
     d = np.zeros(1,dtype='f8')
     d[0] = Q_tot 
-    for ii in constrains:
+    for ii in range(len(constrains)):
         for jj in range(1,len(constrains[ii])):
             cc = np.zeros(Ncharge,dtype='f8')
             cc[constrains[ii][0]] = 1
             cc[constrains[ii][jj]] = -1
-            C = np.vstack(C,cc)
+            C = np.vstack( (C,cc) )
             d = np.append(d,0.0)
+    for ii in range(len(Freeze)):
+        cc = np.zeros(Ncharge,dtype='f8')
+        cc[Freeze[ii][0]] = 1
+        C = np.vstack( (C,cc) )
+        d = np.append(d,Freeze[ii][1])
     CT = C.T
     CT = np.vstack( (CT, np.zeros((C.shape[0],C.shape[0]),dtype="f8") ) )
     
@@ -172,10 +177,24 @@ def RESP_fit_charges(struc,ESP_coor,ESP_pot, Q_tot=0.0, restr=0.001, H_fit=True,
         H_indx = np.where(at_type == "H")[0]
         q_rest[H_indx] = 0.0
     
-    # Calculate initial guess with harmonic constrains
+    # Calculate initial guess with harmonic restrains
     A_work,b_work = add_harmonic_restrain(A,b,q0,q_rest,Ncharge)
     res = np.linalg.solve(A_work,b_work)
-    q = res[:Ncharge]  # initial charges for SCF procedure with hyperbolic constrains
+    q = res[:Ncharge]  # initial charges for SCF procedure with hyperbolic restrains
+    
+    # Results for harmonic restrains
+    q_harmonic = q.copy()
+    pot_harmonic = potential_charge(q_harmonic,R)
+    pot_diff = pot_harmonic - ESP_pot
+    Chi_sqr_harmonic = np.dot(pot_diff,pot_diff)
+    RRMS_harmonic = np.sqrt(Chi_sqr_harmonic/np.dot(ESP_pot,ESP_pot))
+    dipole_harmonic = np.dot(q_harmonic,coor)
+    quadrupole_traceless, quadrupole = ESP_get_quadrupole(coor,q_harmonic)
+    result={"charge_harmonic": q_harmonic,
+            "potential_harmonic": pot_harmonic,"Chi_square_harmonic": Chi_sqr_harmonic,
+            "RRMS_harmonic": RRMS_harmonic,"dipole_harmonic": dipole_harmonic,
+            "quadrupole_harmonic": quadrupole,
+            "quadrupole_traceless_harmonic": quadrupole_traceless}
     
     # SCF calculation procedure
     condition = True
@@ -222,15 +241,55 @@ def RESP_fit_charges(struc,ESP_coor,ESP_pot, Q_tot=0.0, restr=0.001, H_fit=True,
         elif Type=="Transition":
             struc.esp_trans = q.copy()
     
+    # Results 
+    Rcom = struc.get_com()
+    dipole = np.dot(q,coor)
+    quadrupole_traceless, quadrupole = ESP_get_quadrupole(coor,q)
+    
     with position_units("Bohr"):
         coor = Coordinate(coor)
-    result={"charge": q,"coor": coor,"Chi_square": Chi_sqr,"RRMS": RRMS,
-            "steps": count,"q_change": q_change}
+    result.update({"charge": q,"coor": coor,"Chi_square": Chi_sqr,"RRMS": RRMS,
+            "steps": count,"q_change": q_change,"potential_fit": pot_fit,
+            "potential_calc": ESP_pot,"dipole": dipole,"quadrupole": quadrupole,
+            "quadrupole_traceless": quadrupole_traceless})
     
     return result
 
     
+def ESP_get_quadrupole(coor, charge, origin=np.zeros(3,dtype="f8") ):
+    # usualy coordinate system origin in center of mass
+    X = coor[:,0] - origin[0]
+    Y = coor[:,1] - origin[1]
+    Z = coor[:,2] - origin[2]
+    XX = X**2
+    YY = Y**2
+    ZZ = Z**2
     
+    # Calculation of traceless quadrupole defined as Qij = sum_l{ q_l*[3*r_il*rjl - |r_l|^2 * delta_ij] }    
+    # Qxx
+    R = 2*XX - YY - ZZ
+    Qxx = np.dot(charge,R)
+    # Qyy
+    R = 2*YY - XX - ZZ
+    Qyy = np.dot(charge,R)
+    # Qzz
+    R = 2*ZZ - XX - YY
+    Qzz = np.dot(charge,R)
+    # Q xy
+    Qxy = np.dot(charge,X*Y)
+    # Q xz
+    Qxz = np.dot(charge,X*Z)
+    # Q yz
+    Qyz = np.dot(charge,Y*Z)
     
+    Quad_traceless = [Qxx,Qxy,Qxz,Qyy,Qyz,Qzz]
     
+    # Calculation of quadrupole tensor with nonzero trace
+    Qxx = np.dot(charge,XX)
+    Qyy = np.dot(charge,YY)
+    Qzz = np.dot(charge,ZZ)
+    
+    Quad = [Qxx,Qxy,Qxz,Qyy,Qyz,Qzz]
+    
+    return Quad_traceless, Quad
     
--- tests/TEST_ESP/RESP_fit/RESP_fit.py
+++ tests/TEST_ESP/RESP_fit/RESP_fit.py
@@ -8,7 +8,7 @@ import numpy as np
 
 from QChemTool.QuantumChem.read_mine import read_qchem_esp, read_gaussian_esp
 from QChemTool.General.UnitsManager import position_units
-from QChemTool.QuantumChem.esp import RESP_fit_charges
+from QChemTool.QuantumChem.esp import RESP_fit_charges,ESP_get_quadrupole
 from QChemTool.QuantumChem.Classes.structure import Structure
 import matplotlib.pyplot as plt
 
@@ -28,7 +28,22 @@ res = RESP_fit_charges(struc,Points,ESP)
 
 charge = res["charge"]
 coor = res["coor"]
+dipole = res["dipole"]
+quad_traceles = res["quadrupole_traceless"]
+quad = res["quadrupole"]
+quad_traceles = np.array(quad_traceles) #* 2.541765 * 0.52917725
+quad = np.array(quad) #* 2.541765 * 0.52917725
 steps=np.arange(res["steps"])+1
+print("Dipole:",dipole*2.541765)
+print("Quadrupole traceless:")
+print(quad_traceles[0],quad_traceles[1],quad_traceles[2])
+print(quad_traceles[1],quad_traceles[3],quad_traceles[4])
+print(quad_traceles[2],quad_traceles[4],quad_traceles[5])
+
+print("Quadrupole:")
+print(quad[0],quad[1],quad[2])
+print(quad[1],quad[3],quad[4])
+print(quad[2],quad[4],quad[5])
 
 # print results
 for ii in range(struc.nat):
@@ -48,3 +63,34 @@ plt.show()
 plt.plot(steps,res["q_change"])
 plt.title("Mean charge change")
 plt.show()
+
+print("Result from ESP fiting function:")
+quad_test,quad = ESP_get_quadrupole(Coor,Charges)
+quad_test = np.array(quad_test) * 2.541765 * 0.52917725
+print("   XX=",quad_test[0],"   YY=",quad_test[3],"   ZZ=",quad_test[5])
+print("   XY=",quad_test[1],"   XZ=",quad_test[2],"   YZ=",quad_test[4])
+
+print("Result from AMBER RESP fiting:")
+print("   XX=",-55.04654,"   YY=",27.70042,"   ZZ=",27.34612)
+
+print("Result from Gaussian ESP fiting:")
+print("   XX=",-14.790405,"   YY=",7.4598493,"   ZZ=",7.3305554)
+print("   XY=",1.0840891e-13,"   XZ=",-6.4562661e-14,"   YZ=",-1.4252488e-12)
+
+print("Result from Gaussian calculation:")
+print("   XX=",-19.8936,"   YY=",10.0338,"   ZZ=",9.8598)
+print("   XY=",0.0,"   XZ=",0.0,"   YZ=",0.0)
+
+# TRACELESS QUADRUPOLE MOMENT:
+#   XX= -0.14790405D+02   YY=  0.74598493D+01   ZZ=  0.73305554D+01
+#   XY=  0.10840891D-14   XZ= -0.64562661D-15   YZ= -0.14252488D-13
+
+# GAUSSIAN result
+# Traceless Quadrupole moment (field-independent basis, Debye-Ang):
+#   XX=            -19.8936   YY=             10.0338   ZZ=              9.8598
+#   XY=              0.0000   XZ=              0.0000   YZ=              0.0000
+
+# AMBER OUTPUT
+# Quadrupole (Debye*Angst.):
+# Qxx = -55.04654     QYY =  27.70042     QZZ =  27.34612
+# Qxy = -55.04654     QXZ = -55.04654     QYZ = -55.04654
