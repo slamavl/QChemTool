c8c17b7cfc0c3bf61ad1ffa60ba20166a23f319a
Vladislav Slama <slamavl@gmail.com>
2018-01-12 13:34:47 +0100

    Add test of analyzation of MD output

---
+++ tests/MD/Read/Read_mdout.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 11 14:41:55 2018
+
+@author: Vladislav Sláma
+"""
+
+
+# Two alternative ways how to analyze AMBER MD output file (both provide same results) 
+if 0:
+    from QChemTool.QuantumChem.read_mine import read_amber_mdout
+    from QChemTool.QuantumChem.Classes.mdinfo import output_MD_statictic
+    
+    res, inp, avrg, rmsd = read_amber_mdout("03_Pres.out")
+    output_MD_statictic(res, inp, avrg, print_input=True, plot_stat=True, size_RA=10)
+    
+else:
+    from QChemTool.QuantumChem.Classes.mdinfo import analize_amber_mdout
+    
+    analize_amber_mdout("03_Pres.out", size_RA=10)
